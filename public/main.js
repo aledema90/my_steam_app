4587667e0c9220418381eb4ba51b705f40d57cb3
aledema90 <ademarchis90@gmail.com>
2024-07-16 18:01:36 +0200

    modified:   public/main.js

--- public/main.js
+++ public/main.js
@@ -16,13 +16,29 @@ document.getElementById("get-all-games").addEventListener("click", async () => {
   try {
     // Fetch the response from the backend
     const response = await fetch("/games");
+
     // Parse the response as JSON
     const data = response.json();
+
+    // extract the list of games from the response
+    const allGames = data.applist.apps;
+
     // log the entire response in the console
-    console.log(data);
-    // Display the game title in the paragraph
-    document.getElementById("game-title").innerText = data;
+    console.log(allGames);
+
+    // Display the entire list of games in the 'game-list' div
+    const gameListDiv = document.getElementById("game-title");
+    gameListDiv.innerHTML = ""; // Clear any existing content
+
+    const ul = document.createElement("ul"); // Create a new unordered list
+
+    allGames.forEach((allGames) => {
+      const li = document.createElement("li"); // Create a new list item
+      li.innerText = allGames.name; // Set the text content to the game name
+      ul.appendChild(li); // Append the list item to the unordered list
+    });
+    gameListDiv.appendChild(ul); // Append the unordered list to the 'game-list' div
   } catch (error) {
-    console.error("Error fetching the game:", error); // Log any errors that occur
+    console.error("Error fetching the games:", error); // Log any errors that occur
   }
 });
