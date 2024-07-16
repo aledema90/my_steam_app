0434eaf546efd30d8f43c36b34eaf5ac73a499e2
aledema90 <ademarchis90@gmail.com>
2024-07-16 17:49:03 +0200

    modified:   app.js modified:   public/index.html modified:   public/main.js

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 // Import the necessary modules
 
 import express from "express"; // Import Express framework
-// import dotenv from "dotenv"; // Import dotenv for loading environment variables
 import fetch from "node-fetch"; // Import node-fetch for making HTTP requests
-import { steamLink } from "./config.js";
-
-// dotenv.config(); // Load environment variables from .env file
+import { steamLink } from "./config.js"; // Import variables from config file
 
 const app = express(); // Create an instance of Express
 const port = process.env.PORT || 3000; // Define the port on which the server will run or use the PORT environment variable if available
 const STEAM_API_KEY = process.env.STEAM_API_KEY; // Get the Steam API key from environment variables
 
 app.use(express.static("public")); // Serve static files from the 'public' directory
+
+// Define an endpoint to get a random game
+app.get("/games", async (req, res) => {
+  try {
+    // Fetch the list of all games from Steam API
+    const response = await fetch(steamLink);
+    const data = await response.json(); // Parse the response as JSON
+    res.json(data); // Send the random game as a JSON response
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch game list" }); // Handle errors by sending a 500 status
+  }
+});
 
 // Define an endpoint to get a random game
 app.get("/random-game", async (req, res) => {
--- public/main.js
+++ public/main.js
@@ -3,8 +3,25 @@ document.getElementById("get-game").addEventListener("click", async () => {
   try {
     // Fetch a random game from the backend
     const response = await fetch("/random-game");
-    const game = await response.json(); // Parse the response as JSON
-    document.getElementById("game-title").innerText = game.name; // Display the game title in the paragraph
+    // Parse the response as JSON
+    const game = await response.json();
+    // Display the game title in the paragraph
+    document.getElementById("game-title").innerText = game.name;
+  } catch (error) {
+    console.error("Error fetching the game:", error); // Log any errors that occur
+  }
+});
+
+document.getElementById("get-all-games").addEventListener("click", async () => {
+  try {
+    // Fetch the response from the backend
+    const response = await fetch("/games");
+    // Parse the response as JSON
+    const data = response.json();
+    // log the entire response in the console
+    console.log(data);
+    // Display the game title in the paragraph
+    document.getElementById("game-title").innerText = data;
   } catch (error) {
     console.error("Error fetching the game:", error); // Log any errors that occur
   }
