481812055fdbe3ec153db1b69b9b3a6433a10f3d
aledema90 <ademarchis90@gmail.com>
2025-05-29 17:04:43 +0200

    modified:   config.js modified:   vercel.json

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export const steamLink =
   "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
 
-export const STEAM_API_KEY =
-  "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json";
+// La chiave API deve essere impostata come variabile d'ambiente STEAM_API_KEY
